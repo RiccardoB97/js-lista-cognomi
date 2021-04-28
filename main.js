eb6616578e5923cefb000062b3cd154fff108260
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-04-28 13:35:03 +0200

    Add user surname to an arrray

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 //1. chiedi all’utente il cognome
 userSurname = prompt('Insert your surname: ')
+//2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+surnameList = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"]
+surnameList.push(userSurname)
