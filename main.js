0ae7b2278c2e3b78a409c38b26b7370051db275d
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-04-28 13:37:58 +0200

    Shown the position of the surname insterd

--- main.js
+++ main.js
@@ -5,3 +5,5 @@ surnameList = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"]
 surnameList.push(userSurname)
 //3. stampa la lista ordinata alfabeticamente
 surnameList.sort()
+//4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+document.getElementById('position').innerHTML = "The surname you inserted is in position number" + (surnameList.indexOf(userSurname) + 1);
