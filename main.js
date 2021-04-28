b7fcd027c0054f3cafff740d0dea450cae84d2a0
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-04-28 13:35:23 +0200

    Sort array in alphabetical order

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ userSurname = prompt('Insert your surname: ')
 //2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 surnameList = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"]
 surnameList.push(userSurname)
+//3. stampa la lista ordinata alfabeticamente
+surnameList.sort()
