34b2e25931cae93eecd1ad68c7f5427ac291834d
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-05-06 17:18:18 +0200

    Add method to modfy first letter in upper case

--- main.js
+++ main.js
@@ -2,8 +2,9 @@
 userSurname = prompt('Insert your surname: ')
 //2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 surnameList = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"]
-surnameList.push(userSurname)
+userSurnameCaps = userSurname.charAt(0).toUpperCase() + userSurname.slice(1)
+surnameList.push(userSurnameCaps)
 //3. stampa la lista ordinata alfabeticamente
 surnameList.sort()
 //4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
-document.getElementById('position').innerHTML = "The surname you inserted is in position number" + (surnameList.indexOf(userSurname) + 1);
+document.getElementById('position').innerHTML = "The surname you inserted is in position number " + (surnameList.indexOf(userSurnameCaps) + 1);
